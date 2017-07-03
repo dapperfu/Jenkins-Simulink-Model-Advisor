b298001eccf15f589eef036e818da022e0682e55
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 10:38:12 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017 10:38:12 AM

--- resultstr.m
+++ resultstr.m
@@ -1,7 +1,31 @@
 function str = resultstr(result)
+% RESULTSTR - Convert 
+%	DescriptionLine1
+%	DescriptionLine2
+%	DescriptionLine3
+%	DescriptionLine4
+%
+% Syntax:
+%	output1 = resultstr(input1)
+%
+% Inputs:
+%	input1 - InputDescription
+%
+% Outputs:
+%	output1 - OutputDescription
+%
+% Example:
+%	output1 = resultstr(input1)
+%
+% See also: 
+
+% Author: C3038028
+% Email :
+% July 2017
+
+%------------- BEGIN CODE --------------
 
 
 
-
-
+%------------- END CODE ----------------
 end
