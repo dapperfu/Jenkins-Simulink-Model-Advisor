c3afb860d354873bcb11a8a79e5ff7a42c21448d
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 11:19:54 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017 11:19:54 AM

--- junit_xml.m
+++ junit_xml.m
@@ -29,26 +29,29 @@ testsuite.setAttribute('timestamp',strrep(datestr(now, 31), ' ', 'T'));
 % properties.appendChild(property)
 % testsuite.appendChild(properties)
 
-testcase = docNode.createElement('testcase'); 
-testcase.setAttribute('classname', 'ExampleTest');
-testcase.setAttribute('name', 'testOne');
-testcase.setAttribute('time', '0.1');
-if true
-    failure = docNode.createElement('failure');
-    failure.setAttribute('type', 'VerificationFailure');
-    failure.appendChild(docNode.createTextNode('Failure Reason'));
+for i = 1:tests
+    testcase = docNode.createElement('testcase'); 
+    testcase.setAttribute('classname', 'ExampleTest');
+    testcase.setAttribute('name', 'testOne');
+    testcase.setAttribute('time', '0.1');
+    if true
+        failure = docNode.createElement('failure');
+        failure.setAttribute('type', 'VerificationFailure');
+        failure.appendChild(docNode.createTextNode('Failure Reason'));
+    end
+    testcase.appendChild(failure);
+
+    sysout = docNode.createElement('system-out');
+    sysout.appendChild(docNode.createTextNode(''));
+    testcase.appendChild(sysout);
+
+    syserr = docNode.createElement('system-err'); 
+    syserr.appendChild(docNode.createTextNode(''));
+    testcase.appendChild(syserr);
+
+    testsuite.appendChild(testcase);
 end
-testcase.appendChild(failure);
 
-sysout = docNode.createElement('system-out');
-sysout.appendChild(docNode.createTextNode(''));
-testcase.appendChild(sysout)
-
-syserr = docNode.createElement('system-err'); 
-syserr.appendChild(docNode.createTextNode(''));
-testcase.appendChild(syserr)
-
-testsuite.appendChild(testcase);
 docRootNode.appendChild(testsuite);
 
 xmlFileName = [tempname,'.xml'];
