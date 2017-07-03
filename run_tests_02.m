af14ffb536bf9dca246a12e34092f9fd9ed862eb
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-02 21:14:02 -0400

    Changed XML filename.
    Ignore xml results files.

--- run_tests_02.m
+++ run_tests_02.m
@@ -9,7 +9,7 @@ runner = TestRunner.withNoPlugins;
 
 %% Create an XMLPlugin that writes test results to the file myTestResults.xml.
 
-xmlFile = 'myTestResults.xml';
+xmlFile = 'run_test_02.xml';
 p = XMLPlugin.producingJUnitFormat(xmlFile);
 
 %% Add the plugin to the test runner and run the suite.
