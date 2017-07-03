b63ea1da16a665e29b966527da6ad0c5c2153ef2
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 11:10:27 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017 11:10:27 AM

--- simulink_tests_01.m
+++ simulink_tests_01.m
@@ -8,38 +8,38 @@ checks =  ma.CheckCellArray;
 tests = numel(checks);
 successes = sum(cellfun(@(check) check.Success, checks));
 failures  = tests - successes;
-
-%%
-for i=1:numel(checks)
-    check = checks{i};
-    results = check.Result;
-    if ~check.Success
-        switch class(results)
-            case 'cell'
-                for j = 1:numel(results)
-                    result = results{j};
-                    switch class(result)
-                        case 'ModelAdvisor.FormatTemplate'
-                            fprintf('%s\n',result.SubResultStatusText.Content)
-                        case 'cell'
-                            if ~isempty(result{1})
-                                pat = '<[^>]*>';
-                                str = regexprep(result{1}, pat, '');
-                                fprintf('%s\n',str);
-                            end
-                        otherwise
-                            fprintf('Unknown Result Class: %s\n',class(results));
-                            break
-                    end
-                    
-                end
-            case 'char'
-                fprintf('%s\n',results)
-            case 'ModelAdvisor.FormatTemplate'
-                fprintf('%s\n',results.SubResultStatusText.Content)
-            otherwise
-                fprintf('Unknown Results Class: %s\n',class(results));
-                break
-        end
-    end
-end
+% 
+% %%
+% for i=1:numel(checks)
+%     check = checks{i};
+%     results = check.Result;
+%     if ~check.Success
+%         switch class(results)
+%             case 'cell'
+%                 for j = 1:numel(results)
+%                     result = results{j};
+%                     switch class(result)
+%                         case 'ModelAdvisor.FormatTemplate'
+%                             fprintf('%s\n',result.SubResultStatusText.Content)
+%                         case 'cell'
+%                             if ~isempty(result{1})
+%                                 pat = '<[^>]*>';
+%                                 str = regexprep(result{1}, pat, '');
+%                                 fprintf('%s\n',str);
+%                             end
+%                         otherwise
+%                             fprintf('Unknown Result Class: %s\n',class(results));
+%                             break
+%                     end
+%                     
+%                 end
+%             case 'char'
+%                 fprintf('%s\n',results)
+%             case 'ModelAdvisor.FormatTemplate'
+%                 fprintf('%s\n',results.SubResultStatusText.Content)
+%             otherwise
+%                 fprintf('Unknown Results Class: %s\n',class(results));
+%                 break
+%         end
+%     end
+% end
