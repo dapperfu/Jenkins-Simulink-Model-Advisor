b413c78ecd040a5577d4c848d4393d69aa3bbb45
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 00:43:03 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017 12:43:03 AM

--- junit_xml.m
+++ junit_xml.m
@@ -12,6 +12,14 @@ testsuite.setAttribute('name','ts_name');
 testsuite.setAttribute('hostname','THISHOST');
 testsuite.setAttribute('timestamp','2013-05-24T10:23:58');
 
+properties = docNode.createElement('properties'); 
+property = docNode.createElement('property'); 
+property.setAttribute('name', 'java.vendor'); 
+property.setAttribute('value', 'Sun MicroSystems Inc.');
+
+properties.appendChild(property)
+testsuite.appendChild(properties)
+
 testcase = docNode.createElement('testcase'); 
 testcase.setAttribute('classname', 'ExampleTest');
 testcase.setAttribute('name', 'testOne');
@@ -22,6 +30,17 @@ failure.setAttribute('type', 'VerificationFailure');
 failure.appendChild(docNode.createTextNode('Failure Reason'))
 
 testcase.appendChild(failure);
+
+sysout = docNode.createElement('system-out'); 
+sysout.appendChild(docNode.createTextNode(' '));
+
+testcase.appendChild(sysout)
+
+syserr = docNode.createElement('system-err'); 
+syserr.appendChild(docNode.createTextNode(' '));
+testcase.appendChild(syserr)
+
+
 testsuite.appendChild(testcase);
 
 docRootNode.appendChild(testsuite);
