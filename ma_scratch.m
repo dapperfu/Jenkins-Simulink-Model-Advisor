30a4ad3295ea64f9013f8672475e83f36a9fc254
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 13:01:49 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017  1:01:49 PM

--- ma_scratch.m
+++ ma_scratch.m
@@ -2,5 +2,7 @@ model = 'embedded_coder_fixedstep_multirate';
 open_system(model);
 ma = Simulink.ModelAdvisor.getModelAdvisor(model);
 %%
+checks = ma.getCheckAll;
 groups = ma.getGroupAll;
-tasks  = ma.getCheckAll;
+
+tasks  = ma.getTaskAll;
