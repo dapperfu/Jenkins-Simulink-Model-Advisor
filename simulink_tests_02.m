4fc6b6c418a7271657495bf6edcbe3788f53e02d
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 15:05:29 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017  3:05:28 PM

--- simulink_tests_02.m
+++ simulink_tests_02.m
@@ -1,3 +1,15 @@
+% Jenkins Set Environment Variables
+% https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-below
+envs = {'BUILD_NUMBER', 'BUILD_ID', 'BUILD_URL', 'NODE_NAME', 'JOB_NAME', 'BUILD_TAG', 'JENKINS_URL', 'EXECUTOR_NUMBER', 'WORKSPACE'};
+% Alphabetically sort the environmental variables to print.
+envs = sort(envs);
+% Loop through the variables and print their result to the diary.
+for env = envs
+    env = env{1};
+    fprintf('%s: %s\n', env, getenv(env))
+end
+
+
 model = 'embedded_coder_fixedstep_multirate';
 open_system(model);
 ma = Simulink.ModelAdvisor.getModelAdvisor(model);
