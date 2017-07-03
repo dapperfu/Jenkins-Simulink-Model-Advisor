05e512e09bd056f28dc01904820b4802cc49d3d6
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 13:38:15 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017  1:38:15 PM

--- junit_xml.m
+++ junit_xml.m
@@ -28,94 +28,94 @@ end
 % 
 % 
 % 
-% %%
-% tests = numel(checks);
-% successes = sum(cellfun(@(check) check.Success, checks));
-% failures  = tests - successes;
-% %%
-% docNode = com.mathworks.xml.XMLUtils.createDocument('testsuites');
-% docRootNode = docNode.getDocumentElement;
-% %%
-% testsuite = docNode.createElement('testsuite'); 
-% 
-% testsuite.setAttribute('tests', sprintf('%d', tests));
-% testsuite.setAttribute('failures', sprintf('%d', failures));
-% testsuite.setAttribute('errors','0');
-% 
-% testsuite.setAttribute('id','0');
-% testsuite.setAttribute('time','0.1');
-% testsuite.setAttribute('package','Simulink Model Advisor');
-% testsuite.setAttribute('name','Simulink Model Advisor');
-% 
-% testsuite.setAttribute('hostname', getenv('COMPUTERNAME'));
-% testsuite.setAttribute('timestamp',strrep(datestr(now, 31), ' ', 'T'));
-% 
-% properties = docNode.createElement('properties'); 
-% property = docNode.createElement('property'); 
-% property.setAttribute('ModelName', ma.ModelName);
-% properties.appendChild(property)
-% testsuite.appendChild(properties)
-% 
-% testcases = cell(1, tests);
-% for i = 1:tests
-%     testcase = docNode.createElement('testcase'); 
-%     
-%     test = checks{i};
-%     testcase.setAttribute('id', sprintf('%d',i));
+%%
+tests = numel(n_checks);
+successes = sum(cellfun(@(check) check.Success, checks));
+failures  = tests - successes;
+%%
+docNode = com.mathworks.xml.XMLUtils.createDocument('testsuites');
+docRootNode = docNode.getDocumentElement;
+%%
+testsuite = docNode.createElement('testsuite'); 
+
+testsuite.setAttribute('tests', sprintf('%d', tests));
+testsuite.setAttribute('failures', sprintf('%d', failures));
+testsuite.setAttribute('errors','0');
+
+testsuite.setAttribute('id','0');
+testsuite.setAttribute('time','0.1');
+testsuite.setAttribute('package','Simulink Model Advisor');
+testsuite.setAttribute('name','Simulink Model Advisor');
+
+testsuite.setAttribute('hostname', getenv('COMPUTERNAME'));
+testsuite.setAttribute('timestamp',strrep(datestr(now, 31), ' ', 'T'));
+
+properties = docNode.createElement('properties'); 
+property = docNode.createElement('property'); 
+property.setAttribute('ModelName', ma.ModelName);
+properties.appendChild(property)
+testsuite.appendChild(properties)
+
+testcases = cell(1, tests);
+for i = 1:tests
+    testcase = docNode.createElement('testcase'); 
+    
+    test = checks{i};
+    testcase.setAttribute('id', sprintf('%d',i));
+    testcase.setAttribute('classname', test.ID);
+    testcase.setAttribute('name', test.Title);
+    
+    result_str = resultstr(test.Result);
+    result_node = docNode.createTextNode(result_str);
+    if test.Success
+        sysout = docNode.createElement('system-out');
+        sysout.appendChild(result_node);
+        testcase.appendChild(sysout);
+    else
+        failure = docNode.createElement('failure');
+        failure.setAttribute('type', 'VerificationFailure');
+        if isempty(result_str)
+           disp(test.Enable); 
+        end
+        failure.appendChild(result_node);
+        testcase.appendChild(failure);
+    end
+    %%
+    testcases{i} = testcase;
+end
+
+
+for i = 1:tests
+    testsuite.appendChild(testcases{i});
+end
 %     testcase.setAttribute('classname', test.ID);
-%     testcase.setAttribute('name', test.Title);
 %     
-%     result_str = resultstr(test.Result);
-%     result_node = docNode.createTextNode(result_str);
-%     if test.Success
-%         sysout = docNode.createElement('system-out');
-%         sysout.appendChild(result_node);
-%         testcase.appendChild(sysout);
-%     else
+% 
+%     testcase.setAttribute('time', '0.1');
+% 
+%     testcase.setAttribute('classname', 'ExampleTest');
+%     testcase.setAttribute('name', 'testOne');
+%        
+%     if true
 %         failure = docNode.createElement('failure');
 %         failure.setAttribute('type', 'VerificationFailure');
-%         if isempty(result_str)
-%            disp(test.Enable); 
-%         end
-%         failure.appendChild(result_node);
-%         testcase.appendChild(failure);
+%         failure.appendChild(docNode.createTextNode('Failure Reason'));
 %     end
-%     %%
-%     testcases{i} = testcase;
+%     testcase.appendChild(failure);
+% 
+%     sysout = docNode.createElement('system-out');
+%     sysout.appendChild(docNode.createTextNode(''));
+%     testcase.appendChild(sysout);
+% 
+%     syserr = docNode.createElement('system-err'); 
+%     syserr.appendChild(docNode.createTextNode(''));
+%     testcase.appendChild(syserr);
+% 
+%     testsuite.appendChild(testcase);
 % end
-% 
-% 
-% for i = 1:tests
-%     testsuite.appendChild(testcases{i});
-% end
-% %     testcase.setAttribute('classname', test.ID);
-% %     
-% % 
-% %     testcase.setAttribute('time', '0.1');
-% % 
-% %     testcase.setAttribute('classname', 'ExampleTest');
-% %     testcase.setAttribute('name', 'testOne');
-% %        
-% %     if true
-% %         failure = docNode.createElement('failure');
-% %         failure.setAttribute('type', 'VerificationFailure');
-% %         failure.appendChild(docNode.createTextNode('Failure Reason'));
-% %     end
-% %     testcase.appendChild(failure);
-% % 
-% %     sysout = docNode.createElement('system-out');
-% %     sysout.appendChild(docNode.createTextNode(''));
-% %     testcase.appendChild(sysout);
-% % 
-% %     syserr = docNode.createElement('system-err'); 
-% %     syserr.appendChild(docNode.createTextNode(''));
-% %     testcase.appendChild(syserr);
-% % 
-% %     testsuite.appendChild(testcase);
-% % end
-% 
-% docRootNode.appendChild(testsuite);
-% 
-% xmlFileName = [tempname,'.xml'];
-% xmlwrite(xmlFileName,docNode);
-% type(xmlFileName);
+
+docRootNode.appendChild(testsuite);
+
+xmlFileName = [tempname,'.xml'];
+xmlwrite(xmlFileName,docNode);
+type(xmlFileName);
