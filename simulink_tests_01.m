b2b70550270f6864acf531998455082dd2e60be6
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 00:13:02 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017 12:13:02 AM

--- simulink_tests_01.m
+++ simulink_tests_01.m
@@ -6,14 +6,20 @@ ma.run;
 checks =  ma.CheckCellArray;
 for i=1:numel(checks)
     check = checks{i};
+    result = check.Result;
     if ~check.Success
-        if iscell(check.Result)
-            for j = 1:numel(check.Result)
-               fprintf('%02d: %s\n',j,check.Result{j}); 
+        switch class(result)
+            case 'cell'
+            for j = 1:numel(result)
+                if ~isempty(result{j}{1}
+                    fprintf('%02d: %s\n',j,result{j}{1}); 
+                end
             end
-        else
-            fprintf('%s\n',check.Result)
-            
+            case 'char'
+            fprintf('%s\n',result)
+            otherwise
+                fprintf('Unknown Result Class: %s\n',class(result));
+                break
         end
     end
 end
