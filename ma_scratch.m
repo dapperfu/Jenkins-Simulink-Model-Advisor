ecbcc777d09a3e45aaaa17bf559c7d96b54b42b6
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-07-03 13:03:54 -0400

    Development Sprint Auto Commit: Mon, Jul  3, 2017  1:03:53 PM

--- ma_scratch.m
+++ ma_scratch.m
@@ -4,5 +4,7 @@ ma = Simulink.ModelAdvisor.getModelAdvisor(model);
 %%
 checks = ma.getCheckAll;
 groups = ma.getGroupAll;
+group_checks = cellfun(@(group) ma.getCheckForGroup(group), groups, 'UniformOutput', false);
 
 tasks  = ma.getTaskAll;
+task_checks = cellfun(@(task) ma.getCheckForTask(task), tasks, 'UniformOutput', false);
