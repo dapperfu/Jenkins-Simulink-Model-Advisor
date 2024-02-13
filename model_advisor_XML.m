f080331bef6b1bc5218400d568fa8b41a254e28f
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-13 08:57:24 +0100

    Update model_advisor_XML.m

--- model_advisor_XML.m
+++ model_advisor_XML.m
@@ -1,52 +1,60 @@
 % Jenkins Set Environment Variables
-% https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-below
-envs = {'BUILD_NUMBER', 'BUILD_ID', 'BUILD_URL', 'NODE_NAME', 'JOB_NAME', 'BUILD_TAG', 'JENKINS_URL', 'EXECUTOR_NUMBER', 'WORKSPACE'};
-% Alphabetically sort the environmental variables to print.
-envs = sort(envs);
-% Loop through the variables and print their result to the diary.
-for env = envs
-    env = env{1};
-    fprintf('%s: %s\n', env, getenv(env))
+% Define environment variables relevant for Jenkins builds
+jenkinsEnvVariables = {'BUILD_NUMBER', 'BUILD_ID', 'BUILD_URL', 'NODE_NAME', 'JOB_NAME', 'BUILD_TAG', 'JENKINS_URL', 'EXECUTOR_NUMBER', 'WORKSPACE'};
+
+% Alphabetically sort the environmental variables
+jenkinsEnvVariables = sort(jenkinsEnvVariables);
+
+% Loop through the variables and print their values to the diary
+for envVar = jenkinsEnvVariables
+    envVar = envVar{1};
+    fprintf('%s: %s\n', envVar, getenv(envVar))
 end
 
-
+% Model Advisor
 model = 'embedded_coder_fixedstep_multirate';
+
+% Open the Simulink model
 open_system(model);
+
+% Pause for 5 seconds to ensure the model is fully loaded
 pause(5);
+
+% Get the Model Advisor object for the model
 ma = Simulink.ModelAdvisor.getModelAdvisor(model);
+
+% Pause for 5 seconds to ensure Model Advisor initialization
 pause(5);
+
+% Run Model Advisor checks
 ma.run;
 
+% Get the Model Advisor checks
 checks = ma.CheckCellArray;
 n_checks = numel(checks);
 
-% 
-% 
-% 
-%%
-tests = (n_checks);
-successes = sum(cellfun(@(check) check.Success, checks));
-skipped  =  sum(cellfun(@(check) isempty(check.Result), checks));
-failures  = tests - successes - skipped;
+% Initialize counters for successes, failures, and skipped checks
+successes = 0;
+failures = 0;
+skipped = 0;
 
-%%
+% Create XML document for test results
 docNode = com.mathworks.xml.XMLUtils.createDocument('testsuites');
 docRootNode = docNode.getDocumentElement;
-%%
+
+% Create testsuite element
 testsuite = docNode.createElement('testsuite'); 
-
-testsuite.setAttribute('tests', sprintf('%d', tests));
+testsuite.setAttribute('tests', sprintf('%d', n_checks));
 testsuite.setAttribute('failures', sprintf('%d', failures));
-testsuite.setAttribute('errors','0');
-
-testsuite.setAttribute('id','0');
-testsuite.setAttribute('time','0.1');
-testsuite.setAttribute('package','Simulink Model Advisor');
-testsuite.setAttribute('name','Simulink Model Advisor');
-
+testsuite.setAttribute('errors', '0');
+testsuite.setAttribute('id', '0');
+testsuite.setAttribute('time', '0.1');
+testsuite.setAttribute('package', 'Simulink Model Advisor');
+testsuite.setAttribute('name', 'Simulink Model Advisor');
 testsuite.setAttribute('hostname', getenv('COMPUTERNAME'));
-testsuite.setAttribute('timestamp',strrep(datestr(now, 31), ' ', 'T'));
+testsuite.setAttribute('timestamp', strrep(datestr(now, 31), ' ', 'T'));
 
+% Add properties element to testsuite
 properties = docNode.createElement('properties'); 
 property = docNode.createElement('property'); 
 property.setAttribute('name', 'ModelName'); 
@@ -54,46 +62,44 @@ property.setAttribute('value', ma.ModelName);
 properties.appendChild(property);
 testsuite.appendChild(properties);
 
-testcases = cell(1, tests);
-for i = 1:tests
+% Create testcases for each Model Advisor check
+testcases = cell(1, n_checks);
+for i = 1:n_checks
     testcase = docNode.createElement('testcase'); 
-    
     check = checks{i};
-    
     testcase.setAttribute('classname', check.ID);
     testcase.setAttribute('name', check.Title);
-    
-    testcase.setAttribute('time','0.1');
+    testcase.setAttribute('time', '0.1');
 
-    
-    result_str = resultstr(check.result);
-    result_node = docNode.createTextNode(result_str);
+    % Check if the check was successful, failed, or skipped
     if check.Success
-        sysout = docNode.createElement('system-out');
-        sysout.appendChild(result_node);
-        %testcase.appendChild(sysout);
+        successes = successes + 1;
     elseif isempty(check.Result)
-        skipped = docNode.createElement('skipped');
-        testcase.appendChild(skipped);        
+        skipped = skipped + 1;
     else
+        failures = failures + 1;
         failure = docNode.createElement('failure');
         failure.setAttribute('type', 'VerificationFailure');
-        failure.appendChild(result_node);
+        failure.appendChild(docNode.createTextNode(resultstr(check.Result)));
         testcase.appendChild(failure);
     end
-    
+
     testcases{i} = testcase;
 end
 
-for i = 1:tests
+% Add testcases to testsuite
+for i = 1:n_checks
     testsuite.appendChild(testcases{i});
 end
 
+% Add testsuite to document root
 docRootNode.appendChild(testsuite);
 
-xmlFileName = [model,'.xml'];
-xmlwrite(xmlFileName,docNode);
+% Write XML file with test results
+xmlFileName = [model, '.xml'];
+xmlwrite(xmlFileName, docNode);
 
+% Exit MATLAB with success status code if running in Jenkins environment
 if ~isempty(getenv('BUILD_NUMBER'))
     exit(0);
 end
